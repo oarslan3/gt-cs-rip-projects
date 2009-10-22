e588a14fff149467c09e1ab486c39c46004f9ef9
alexgcunningham <alexgcunningham@e642834e-98c8-11de-b255-e1213ca11573>
2009-10-22 21:10:52 +0000

    Added a new tab to modifiy for using walkers and integrated it.  Currently just a copy of the usual planning tab

---
+++ motion/RST/Tabs/WalkPlanTab.h
@@ -0,0 +1,63 @@
+//---------------------------------------------------------------------
+//  Copyright (c) 2009 Mike Stilman
+//  All Rights Reserved.
+//
+//  Permission to duplicate or use this software in whole or in part
+//  is only granted by consultation with the author.
+//
+//    Mike Stilman              mstilman@cc.gatech.edu
+//
+//	  Robotics and Intelligent Machines
+//    Georgia Tech
+//--------------------------------------------------------------------
+
+#ifndef WALK_PLAN_TAB
+#define WALK_PLAN_TAB
+
+#include "RSTTab.h"
+#include "../Tools/Matrix.h"
+#include "../Tools/Robot.h"
+#include "../Tools/Planner.h"
+
+class WalkPlanTab : public RSTTab
+{
+public:
+	WalkPlanTab(){};
+    WalkPlanTab(wxWindow * parent, wxWindowID id = -1,
+             const wxPoint & pos = wxDefaultPosition,
+             const wxSize & size = wxDefaultSize,
+             long style = wxTAB_TRAVERSAL);
+	virtual ~WalkPlanTab(){}
+
+	int rrtStyle;
+	bool greedyMode;
+	bool connectMode;
+	bool showProg;
+	Planner *planner;
+	
+	wxSizer* sizerFull;
+
+	int robotID;
+
+	wxTextCtrl *timeText;
+
+	rstate startConf;
+	rstate goalConf;
+
+	// public vars to capture external selection stuff (should move these higher somethime)
+	Object* selectedObject;
+	Robot* selectedRobot;
+	Link* selectedLink;
+
+	void OnSlider(wxCommandEvent &evt);
+	void OnRadio(wxCommandEvent &evt);
+	void OnButton(wxCommandEvent &evt);
+	void OnCheckBox(wxCommandEvent &evt);
+	void SetTimeline();
+	void RSTStateChange();
+
+	DECLARE_DYNAMIC_CLASS(WalkPlanTab)
+	DECLARE_EVENT_TABLE()
+};
+
+#endif
